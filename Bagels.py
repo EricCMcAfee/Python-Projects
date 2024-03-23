a4647229b72b359fe4f7fa08067a13e0a5d072d7
EricCMcAfee <eric.c.mcafee@gmail.com>
2024-03-23 07:51:53 -0600

    starting project 1 - bagels

---
+++ Bagels.py
@@ -0,0 +1,15 @@
+"""Bagels, by Al Sweigart a;@inbentwith python.com
+A deductive logic game where you must guess a number based on clues.
+View this code at https://nostarch.com/big-book-small-pyton-projects
+A version of this game is featured in teh book "Invent Your Own Computer Games wit Python" https://nostarch.com/inventwithpython
+Tags: short, game, puzzle"""
+
+import random
+
+NUM_DIGITS = 3 #Try setting to another number
+MAX_GUESSES = 10 #Try setting to another number
+
+def main():
+    print("Bagels, a deductive logic game. By Al Sweigart")
+
+main()
